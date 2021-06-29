05bdb98b85ad6b54b4c0d2c3053345508fcdd05f
Victor Salajan <victorsalajan29@gmail.com>
2021-06-29 18:06:16 +0300

    Added function for displaying EU capitals, by scraping them from a wiki page Added generated map: european_capitals.html Small frontend updates

--- main.py
+++ main.py
@@ -3,19 +3,62 @@ from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 import folium
 from folium.plugins import MarkerCluster
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/display_euro_capitals/')
+def display_euro_capitals():
+    try:
+        return render_template('european_capitals.html')
+    except:
+        url = 'https://en.wikipedia.org/wiki/Template:Capital_cities_of_European_Union_member_states'
+        page = urlopen(url)
+        html = page.read().decode('utf-8')
+        soup = BeautifulSoup(html, 'html.parser')
+
+        tbodies = soup.find_all('tbody')
+
+        countries_and_capitals = []
+        for i, tbody in enumerate(tbodies):
+            if i == 0:
+                pass
+            else:
+                tds = tbody.find('td')
+                ahrefs = tds.find_all('a')
+                country = ahrefs[0]['title']
+                capital = ahrefs[1]['title']
+                countries_and_capitals.append((country, capital))
+
+        geolocator = Nominatim(user_agent='map_scraped_capitals')
+
+        m = folium.Map()
+        for loc_tuple in countries_and_capitals:
+            geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+            country, capital = loc_tuple[0], loc_tuple[1]
+            location = {'country': country, 'city': capital}
+            coordinates = geocode(location)
+            lat, lon = coordinates.point.latitude, coordinates.point.longitude
+            tooltip = f'{country}: {capital}'
+            folium.Marker([lat, lon], tooltip=tooltip).add_to(m)
+        m.save('templates/european_capitals.html')
+        return render_template('european_capitals.html')
+
 global_nr_of_points = []
 points = {}
-@app.route('/', methods=['POST', 'GET'])
-def home():
+@app.route('/choose_points_nr/', methods=['POST', 'GET'])
+def choose_points_nr():
     if request.method == "POST":
         nr_of_points = request.form['nr_of_points']
         global_nr_of_points.append(nr_of_points)
 
         return redirect(url_for('pick_locations'))
-    return render_template('home.html')
+    return render_template('choose_points_nr.html')
 
 @app.route('/pick_locations/', methods=['POST', 'GET'])
 def pick_locations():
